c5bd685e1896553040e6998fbd53b5a97cafc386
Christopher Kyle Horton <christhehorton@gmail.com>
2013-01-14 17:56:55 -0500

    Update copyright years on Python files to 2013.

--- cells.py
+++ cells.py
@@ -1,5 +1,5 @@
 # This file is part of MyKaryotes.
-# Copyright (C) 2012  Christopher Kyle Horton <christhehorton@gmail.com>
+# Copyright (C) 2013  Christopher Kyle Horton <christhehorton@gmail.com>
 
 # MyKaryotes is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
